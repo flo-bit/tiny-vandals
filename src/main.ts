11bd5a122ff3bea76bc6b73bb86f97c33a5b68cf
Florian <45694132+flo-bit@users.noreply.github.com>
2025-01-18 14:05:18 +0100

    commit

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import * as THREE from "three";
 import { setup, update } from "./scene";
+import { Application, Assets, Sprite } from "pixi.js";
 
 (async () => {
   const scene = new THREE.Scene();
@@ -8,8 +9,6 @@ import { setup, update } from "./scene";
     75,
     window.innerWidth / window.innerHeight,
   );
-
-  camera.position.z = 5;
 
   const renderer = new THREE.WebGLRenderer();
   renderer.setSize(window.innerWidth, window.innerHeight);
@@ -39,6 +38,42 @@ import { setup, update } from "./scene";
     total: 0,
   };
 
+  // Create a PixiJS application.
+  const app = new Application();
+
+  // Intialize the application.
+  await app.init({ background: "#ff99bb" });
+
+  const bunnyTexture = await Assets.load("https://pixijs.com/assets/bunny.png");
+  document.body.appendChild(app.canvas);
+
+  // Create a new Sprite from an image path
+  const bunny = new Sprite(bunnyTexture);
+
+  // Add to stage
+  app.stage.addChild(bunny);
+
+  // Center the sprite's anchor point
+  bunny.anchor.set(0.5);
+  bunny.scale.set(10);
+
+  // Move the sprite to the center of the screen
+  bunny.x = 0;
+  bunny.y = 0;
+
+  // add a cube to the threejs scene
+  const material = new THREE.MeshBasicMaterial({
+    color: 0xffffff,
+    side: THREE.DoubleSide,
+  });
+  const cube = new THREE.Mesh(new THREE.PlaneGeometry(2, 1), material);
+  cube.position.z = -2;
+  scene.add(cube);
+
+  // set texture of the cube to be the pixijs canvas
+  const texture = new THREE.CanvasTexture(app.canvas);
+  material.map = texture;
+
   await setup(props);
 
   const animate = async () => {
@@ -49,8 +84,34 @@ import { setup, update } from "./scene";
 
     await update(props);
 
+    // rotate the cube
+    cube.rotation.y += props.delta;
+
+    // render the scene
     renderer.render(scene, camera);
+
+    // render the pixijs canvas
+    app.render();
+
+    texture.needsUpdate = true;
   };
+
+  let isPixi = true;
+  // add event listener to window for keydown
+  window.addEventListener("keydown", (event) => {
+    if (event.key === " ") {
+      if (isPixi) {
+        // switch between pixijs and threejs
+        app.canvas.style.display = "none";
+        renderer.domElement.style.display = "block";
+      } else {
+        // switch between pixijs and threejs
+        app.canvas.style.display = "block";
+        renderer.domElement.style.display = "none";
+      }
+      isPixi = !isPixi;
+    }
+  });
 
   animate();
 })();
--- src/scene.ts
+++ src/scene.ts
@@ -4,20 +4,21 @@ import { Props } from "./types";
 let icosphere: THREE.Mesh;
 
 export const setup = async ({ scene }: Props) => {
-  const geometry = new THREE.IcosahedronGeometry(1, 1);
+  const geometry = new THREE.BoxGeometry(10, 2, 10);
 
   const material = new THREE.MeshStandardMaterial({
-    color: 0x0077ff,
+    color: 0x00ff00,
     flatShading: true,
+    side: THREE.DoubleSide,
   });
 
   icosphere = new THREE.Mesh(geometry, material);
   scene.add(icosphere);
 
-  scene.add(new THREE.DirectionalLight());
+  // add point light
+  scene.add(new THREE.PointLight(0xffffff, 4, 100));
 };
 
 export const update = async ({ delta }: Props) => {
-  icosphere.rotation.x += delta;
   icosphere.rotation.y += delta;
 };
