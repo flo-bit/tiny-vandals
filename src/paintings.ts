9b1b2149c75a505d81b3e2b20418dce0f7a12dbf
Florian <45694132+flo-bit@users.noreply.github.com>
2025-01-18 18:08:09 +0100

    add paintings

---
+++ src/paintings.ts
@@ -0,0 +1,115 @@
+import * as PIXI from "pixi.js";
+
+const paintingData = [
+    {
+        x: 1000,
+        y: 600,
+        width: 768 / 2,
+        height: 1087 / 2,
+        image: "/tiny-vandals/paintings/banksy.jpg",
+    },
+    {
+        x: 3000,
+        y: 600,
+        width: 512,
+        height: 512,
+        image: "/tiny-vandals/paintings/mondrian.jpg",
+    },
+    {
+        x: 5000,
+        y: 600,
+        width: 800,
+        height: 947,
+        image: "/tiny-vandals/paintings/girl-earing.jpg",
+    },
+    {
+        x: 7000,
+        y: 600,
+        width: 640,
+        height: 954,
+        image: "/tiny-vandals/paintings/mona-lisa.jpg.webp",
+    },
+    {
+        x: 9000,
+        y: 600,
+        width: 2140 / 4,
+        height: 2699 / 4,
+        image: "/tiny-vandals/paintings/nebelmeer.jpg",
+    },
+    {
+        x: 11000,
+        y: 600,
+        width: 810 / 1.5,
+        height: 1024 / 1.5,
+        image: "/tiny-vandals/paintings/scream.jpg",
+    },
+    {
+        x: 13400,
+        y: 600,
+        width: 1200,
+        height: 950,
+        image: "/tiny-vandals/paintings/starry-night.jpg",
+    },
+    {
+        x: 15000,
+        y: 600,
+        width: 251 * 2,
+        height: 251 * 2,
+        image: "/tiny-vandals/paintings/willie.jpg",
+    },
+];
+
+class Painting extends PIXI.Sprite {
+    damage: number = 0;
+}
+
+const paintings: Painting[] = [];
+
+export const setupPaintings = async (
+    app: PIXI.Application,
+    container: PIXI.Container,
+) => {
+    for (const element of paintingData) {
+        const sprite = new Painting(await PIXI.Assets.load(element.image));
+        sprite.x = element.x;
+        sprite.y = element.y;
+        sprite.width = element.width;
+        sprite.height = element.height;
+
+        sprite.anchor.set(0.5);
+
+        container.addChild(sprite);
+
+        paintings.push(sprite);
+    }
+};
+
+export const nearestPainting = (x: number, y: number) => {
+    let nearest: Painting | null = null;
+    let nearestDistance = Infinity;
+    for (const painting of paintings) {
+        const distance = Math.sqrt(
+            (painting.x - x) ** 2 + (painting.y - y) ** 2,
+        );
+        if (distance < nearestDistance) {
+            nearestDistance = distance;
+            nearest = painting;
+        }
+    }
+    return nearest;
+};
+
+export const addDamage = (x: number, y: number) => {
+    // check if x, y is in painting
+    for (const painting of paintings) {
+        if (
+            x > painting.x - painting.width / 2 &&
+            x < painting.x + painting.width / 2 &&
+            y > painting.y - painting.height / 2 &&
+            y < painting.y + painting.height / 2
+        ) {
+            painting.damage++;
+            console.log(painting.damage);
+        }
+    }
+};
--- src/pixi-app.ts
+++ src/pixi-app.ts
@@ -6,18 +6,9 @@ import {
     Renderer,
     Sprite,
 } from "pixi.js";
+import { setupPaintings } from "./paintings";
 
 const spiderTexture = await Assets.load("/tiny-vandals/images/Monster1.png");
-const paintingTextures = [
-    await Assets.load("/tiny-vandals/images/painting01.png"),
-    await Assets.load("/tiny-vandals/images/painting02.png"),
-    await Assets.load("/tiny-vandals/images/painting03.png"),
-];
-
-type Painting = {
-    damage: number;
-    sprite: Sprite;
-};
 
 type Enemy = {
     fadeFrame: number | null;
@@ -28,15 +19,14 @@ type Enemy = {
 
 type TinyVandalsWall = {
     app: Application<Renderer>;
-    paintings: unknown[];
     enemies: Enemy[];
 };
 
 let wall: TinyVandalsWall | null = null;
 
 export async function createTinyVandalsWall({
-    width = 2048 * 2,
-    height = 2048 * 0.05 * 2,
+    width = 2048 * 8,
+    height = 2048 * 0.075 * 8,
 }: {
     width?: number;
     height?: number;
@@ -51,34 +41,14 @@ export async function createTinyVandalsWall({
         background: "#7b1c31",
     });
 
-    document.body.appendChild(app.canvas);
+    // document.body.appendChild(app.canvas);
 
     const container = new Container();
     container.position.set(0, height);
     container.scale.y = -1;
     app.stage.addChild(container);
 
-    const paintings: Painting[] = [];
-    for (let index = 0; index < 3; index++) {
-        // Create a new Sprite from an image path
-        const sprite = new Sprite(paintingTextures[index]);
-
-        // Add to stage
-        container.addChild(sprite);
-        const painting = {
-            damage: 0,
-            sprite,
-        };
-
-        // Center the sprite's anchor point
-        sprite.anchor.set(0.5);
-        sprite.scale.set(0.6);
-
-        // Move the sprite to the center of the screen
-        sprite.x = (width / 4) * index + 300;
-        sprite.y = height / 2;
-        paintings.push(painting);
-    }
+    setupPaintings(app, container);
 
     const enemies: Enemy[] = [];
     for (let index = 0; index < 1; index++) {
@@ -104,10 +74,8 @@ export async function createTinyVandalsWall({
         enemies.push(enemy);
     }
 
-
     wall = {
         app,
-        paintings,
         enemies,
     };
     return wall;
